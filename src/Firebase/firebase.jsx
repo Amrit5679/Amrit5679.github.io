f99b1d0bf79aa8d2ef197c60f453c84abc3b9ed6
Amrit Mishra <mishraamrit929@gmail.com>
2024-06-29 21:46:44 +0530

    Changed configuration

--- src/Firebase/firebase.jsx
+++ src/Firebase/firebase.jsx
@@ -10,13 +10,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_API_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  apiKey: 'AIzaSyCF5ZTxU2izIKvqUkVKSjILufVMm6545oo',
+  authDomain: 'event-management-system-fed0c.firebaseapp.com',
+  projectId: 'event-management-system-fed0c',
+  storageBucket: 'event-management-system-fed0c.appspot.com',
+  messagingSenderId: '378030448237',
+  appId: '1:378030448237:web:c5bd108c35618e63e9e964',
+  measurementId: 'G-L46MQETGMK'
 };
 console.log({firebaseConfig})
 
